32445a48bab4d2e17c646e8f27cbcf7a77175c40
Cedric Nicoloso <cnicoloso@octo.com>
2018-02-06 16:34:45 +1100

    Take into account multiple args for console.log

--- fileLogger.js
+++ fileLogger.js
@@ -1,13 +1,12 @@
 function getFileLogger() {
 
   const fs = require('fs')
-  const util = require('util')
   const log_file = fs.createWriteStream(__dirname + '/out.log', { flags : 'a' })
   const log_stdout = process.stdout
 
-  return function(logLine) {
-    log_file.write(`${new Date(Date.now()).toLocaleString()} - ${util.format(logLine)}\n`)
-    log_stdout.write(util.format(logLine) + '\n')
+  return function(...logLine) {
+    log_file.write(`${new Date(Date.now()).toLocaleString()} - ${logLine.join(' ')}\n`)
+    log_stdout.write(logLine.join(' ') + '\n')
   }
 }
 
